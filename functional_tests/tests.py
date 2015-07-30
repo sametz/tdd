b78ec7a9f713b9b9c0465cc24adf3cef34622e26
Geoffrey Sametz <geoffreysametz@Geoffreys-MBP.home>
2015-07-29 22:35:24 -0400

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,9 +4,25 @@ from selenium.webdriver.common.keys import Keys
 
 
 import unittest
+import sys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):  # Now using LiveServerTestCase, not unittest.TestCase
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -16,7 +32,7 @@ class NewVisitorTest(StaticLiveServerTestCase):  # Now using LiveServerTestCase,
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)  # changed p134
         self.browser.set_window_size(1024, 768)
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -39,7 +55,7 @@ class NewVisitorTest(StaticLiveServerTestCase):  # Now using LiveServerTestCase,
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)  # from LiveServerTestCase--replaces 'http://localhost:8000'
+        self.browser.get(self.server_url)  # changed p134
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -83,7 +99,7 @@ class NewVisitorTest(StaticLiveServerTestCase):  # Now using LiveServerTestCase,
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)  # changed p134
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
