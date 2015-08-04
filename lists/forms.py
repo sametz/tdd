92e2981869a4b9a508af0dfe753f0072a510bf0f
Geoffrey Sametz <geoffreysametz@Geoffreys-MBP.home>
2015-08-04 06:46:22 -0400

    End of Chapter 12; skip removed, forms and their test tweaked

--- lists/forms.py
+++ lists/forms.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
+
 EMPTY_ITEM_ERROR = "You can't have an empty list item"  # Assume p.197 this goes here
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
@@ -39,3 +40,7 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
+    
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -49,6 +49,12 @@ class ExistingListItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
+    def test_form_save(self):  # Not sure this goes here...p222
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
+
 
 
 
